dc957b463c823b047fce23578f79afa7e54334f8
Your Name <you@example.com>
2020-06-26 19:11:29 -0500

    first commit

---
+++ Assets/script-codigo/ButtonConsecutiveEvents.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonConsecutiveEvents : MonoBehaviour
+{
+    public UnityEvent[] events;
+
+    private int _clickCount;
+
+    public void Click()
+    {
+        events[_clickCount].Invoke();
+
+        _clickCount++;
+    }
+}
---
+++ Assets/script-codigo/WebcamTexture.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebcamTexture : MonoBehaviour
+{
+    // Starts the default camera and assigns the texture to the current renderer
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
